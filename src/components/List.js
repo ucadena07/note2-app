f2a2ba156d1273b8ddcd184d8fd1631c9d46f635
Ulises Cadena <ucadena07@gmail.com>
2020-04-01 15:59:44 -0500

    finishing nav components

---
+++ src/components/List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class List extends React.Component {
+  render() {
+    return (
+      <div className="list-container">
+        List Componets
+      </div>
+    );
+  }
+
+}
+
+export default List;

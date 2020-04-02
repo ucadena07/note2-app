4b607d96ae50220c7bc096823035655dc4304671
Ulises Cadena <ucadena07@gmail.com>
2020-04-02 13:47:22 -0500

    set up list components

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import NoteCard from './NoteCard';
 
 class List extends React.Component {
+  
   componentWillMount() {
     this.props.getNotes();
   }
 
   render() {
+    const { notes } = this.props;
+
+    const cards = notes.map((note, index) => {
+      return (
+        <NoteCard
+          key={index}
+          index={index}
+          note={note}
+        />
+      );
+    });
+
     return (
       <div className="list-container">
-        List Componets
+        {cards}
       </div>
     );
   }

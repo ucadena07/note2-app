a28240b81345b4d04e9de285333f2daf17257044
Ulises Cadena <ucadena07@gmail.com>
2020-04-02 17:03:32 -0500

    note form

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
     super();
     this.state = {
       showNote: false,
-      notes: []
+      notes: [],
+      note: {}
     };
   }
 
@@ -27,18 +28,22 @@ class App extends Component {
     .catch((err) => console.log(err.response.data));
   }
 
-  getNote = () => {
-    console.log('Clicked');
+  getNote = (id) => {
+    axios.get(urlFor(`notes/${id}`))
+    .then((res) => this.setState({ note: res.data, showNote: true }))
+    .catch((err) => console.log(err.response.data));
   }
 
   render() {
-    const { showNote, notes } = this.state;
+    const { showNote, notes, note } = this.state;
 
     return (
       <div className="App">
         <Nav toggleNote={this.toggleNote} showNote={showNote} />
         { showNote ?
-         <Note /> 
+         <Note
+          note={note}
+        /> 
         :
          <List
           getNotes={this.getNotes}
